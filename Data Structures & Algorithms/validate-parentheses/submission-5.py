b41608aa59e1cb4f0faa4c9688d2f27730095515
John Paiva <134613288+Jpp313@users.noreply.github.com>
2026-08-30 09:57:57 -0400

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        open_p = [] 
+        closeToOpen = {"}" : "{", "]" : "[", ")" : "("}
+
+        for c in s:
+            
+            if c in closeToOpen: # close paranthese condition
+                if open_p and open_p[-1] == closeToOpen[c]:
+                    open_p.pop()
+            else:
+                open_p.append(c)
+        
+        return len(open_p) == 0
