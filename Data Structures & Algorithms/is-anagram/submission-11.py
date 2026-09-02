7e8279b4c5baebfbe34faaa785c0223557c77cdd
John Paiva <134613288+Jpp313@users.noreply.github.com>
2026-09-02 17:24:45 -0400

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t): # two strings with diff lengths can never be anagrams
+            return False
+
+        count = [0] * 26 # 26 letters of alphabet since we are bounded to just lower chars
+
+        for i in range(len(s)): # loop will change freqeuencys up and down if chars are same end result should be 0 in all places meaning every char got added and deleted and none were left behind meaning one string had a diff freq of char than the other
+            count[ord(s[i]) - ord('a')] += 1 
+            count[ord(t[i]) - ord('a')] -= 1
+
+        
+        for val in count:
+            if val != 0:
+                return False
+        return True
