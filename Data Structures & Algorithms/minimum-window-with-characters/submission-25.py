518d46aff6c2cda7548c3effc2780108cc634228
John Paiva <134613288+Jpp313@users.noreply.github.com>
2026-09-02 18:06:21 -0400

    Add: minimum-window-with-characters - submission-25

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-25.py
@@ -0,0 +1,43 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        
+        if len(t) > len(s):
+            return ""
+        
+        res = [-1, -1]
+        resLen = float("infinity")
+
+        window = {}
+        countT = {}
+
+        for char in t:
+            countT[char] = 1 + countT.get(char, 0)
+        
+        l = 0
+        have = 0
+        need = len(countT)
+        for r in range(len(s)):
+            c = s[r]
+
+            window[c] = 1 + window.get(c, 0)
+
+            if c in countT and window[c] == countT[c]: # build have as we find the same chars in each
+                have += 1
+
+            while have == need: # when we find valid substring
+                length = r - l + 1
+                if length < resLen:
+                    res = l , r
+                    resLen = length
+
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] + 1 == countT[s[l]]:
+                    have -= 1
+                
+                l += 1
+                    
+        l , r  = res
+        if resLen != float("infinity"):
+            return s[l : r + 1]
+        else:
+            return ""
