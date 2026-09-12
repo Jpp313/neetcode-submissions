f1ea11c365bc4d4e3d412461265cf39f2ac042bb
John Paiva <134613288+Jpp313@users.noreply.github.com>
2026-09-12 15:28:17 -0400

    Add: eating-bananas - submission-13

---
+++ Data Structures & Algorithms/eating-bananas/submission-13.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+        l = 1
+        r = max(piles)
+        res = r
+        # adjust eating rate (mid) up or down if we eat too fast or too slow
+        while l <= r:
+            k = (l + r) // 2
+            hours = 0
+            for p in piles:
+                hours += math.ceil(p / k)
+            
+            if hours <= h: # ate too fast
+                res = k # only store valid k values
+                r = k - 1
+            elif hours > h: # ate too slow
+                l = k + 1
+          
+        return res
