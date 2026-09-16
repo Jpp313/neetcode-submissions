4905f09dbc0c817d56469b45d0b0817bceceec44
John Paiva <134613288+Jpp313@users.noreply.github.com>
2026-09-16 10:01:38 -0400

    Add: buy-and-sell-crypto - submission-16

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-16.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        l = 0
+        r = 1
+        max_profit = 0
+        leftmin = float("infinity")
+        while r < len(prices):
+
+            leftmin = min(leftmin, prices[l])
+            if leftmin < prices[r]: # make a profit
+                max_profit = max(max_profit, prices[r] - prices[l])
+                r += 1
+            else:
+                l += 1
+                r += 1
+
+        return max_profit
