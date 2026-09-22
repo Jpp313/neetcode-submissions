f2d74a29b1c4ffc6839a3566b30570c1c6a1c8ef
John Paiva <134613288+Jpp313@users.noreply.github.com>
2026-09-22 09:25:42 -0400

    Add: validate-parentheses - submission-11

---
+++ Data Structures & Algorithms/validate-parentheses/submission-11.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        closeToOpen = { "}" : "{", ")" : "(", "]" : "["}
+
+        stack = [] # holds all open brackets
+
+        for sym in s:
+            if sym in closeToOpen: # it is a close symbol
+                open_sym = stack.pop()
+                if open_sym != closeToOpen[sym]:
+                    return False
+            else: # add open sym to stack
+                stack.append(sym)
+
+        return len(stack) == 0
+            
